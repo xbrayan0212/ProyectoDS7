ea82b28494b70422aefedee8d7d25afe0c8170df
xbrayan0212 <xbrayan.tobii02@gmail.com>
2024-08-23 11:58:49 -0500

    feat: Añade la funcionalidad de logout

---
+++ usuarios/logout.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+session_unset();
+session_destroy();
+
+// Responder con un mensaje de éxito en formato JSON
+$response = array('success' => true);
+echo json_encode($response);
+header("Location: ../public/index.html");
+?>
