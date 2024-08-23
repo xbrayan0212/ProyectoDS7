ecfc7c66bfaa09934068371d5027a9029b681635
xbrayan0212 <xbrayan.tobii02@gmail.com>
2024-08-23 18:04:19 -0500

    feat: Añade formulario de cambio de contraseña

---
+++ includes/general/form_cambio_password.php
@@ -0,0 +1,33 @@
+
+<div class="container_password">
+    <h2>Cambiar Contraseña</h2>
+    <div class="img_password_cambio">
+        <img   src="../../public/imagenes/clave.png" alt="">
+    </div>
+    <div class="error_mensaje"></div>
+    <form action="procesar_cambio_password.php" method="POST" class="form-cambio_password"  onsubmit="changePassword(event)" >
+        <div class="form-group">
+            <label for="actual_password">Contraseña Actual:</label>
+            <input type="password" id="actual_password" name="actual_password" required>
+            <div id="errorMensajeNew" class="errorMensajeNew"></div>
+        </div>
+        <div class="form-group">
+            <label for="new_password">Nueva Contraseña:</label>
+            <input type="password" id="new_password" name="new_password" required>
+        </div>
+        <div class="form-group">
+            <label for="confirm_password">Confirmar Nueva Contraseña:</label>
+            <input type="password" id="confirm_password" name="confirm_password" required>
+        </div>
+        <button type="submit" class="btn">Cambiar Contraseña</button>
+    </form>
+    <div id="modalExito" class="modal" style="display: none;">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <p>¡Contraseña cambiada exitosamente!</p>
+        </div>
+    </div>
+
+</div>
+
+<script src="../js/scripts.js"></script>
