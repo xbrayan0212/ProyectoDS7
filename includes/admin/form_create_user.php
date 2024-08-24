e87f8a6038f49fba7332731e20815aae3c2dc393
xbrayan0212 <xbrayan.tobii02@gmail.com>
2024-08-24 18:50:20 -0500

    feat: Modificaciones en el formulario de creación de usuarios

--- includes/admin/form_create_user.php
+++ includes/admin/form_create_user.php
@@ -1,29 +1,24 @@
-<div class="container_create_user">
-    <section class="conteiner_user">
-        <div class="img_create_user">
+<div class="container">
+    <section class="form-container form_creat_user">
+        <div class="img-container img_container_create_user">
             <img src="../../public/imagenes/user_new.png" alt="Crear Usuario">
         </div>
-      
-</section>
-<section class="section_form_user">
-<div  class="error_mensaje"></div>
-    <form action="../../usuarios/create_user.php" method="POST" class="form-create_user" onsubmit="createUser(event)">
-    <h2 class="h2_user_form">Crear Usuario</h2>
-            <div class="form-group_user">
+        <form action="../../usuarios/create_user.php" method="POST" class="form" onsubmit="createUser(event)">
+        <div class="error-mensaje"></div>
+            <h2 class="h2-user-form">Crear Usuario</h2>
+            <div class="form-group">
                 <label for="username">Nombre de Usuario:</label>
                 <input type="text" id="username" name="username" required>
-                <div id="errorMensajeUsername" class="errorMensaje"></div>
             </div>
-            <div class="form-group_user">
+            <div class="form-group">
                 <label for="password">Contraseña:</label>
                 <input type="password" id="password" name="password" required>
-                <div id="errorMensajePassword" class="errorMensaje"></div>
             </div>
-            <div class="form-group_user">
+            <div class="form-group">
                 <label for="confirm_password">Confirmar Contraseña:</label>
                 <input type="password" id="confirm_password" name="confirm_password" required>
             </div>
-            <div class="form-group_user">
+            <div class="form-group">
                 <label for="account_type">Tipo de Cuenta:</label>
                 <select id="account_type" name="account_type" required>
                     <option value="A">Administrador</option>
@@ -33,13 +28,13 @@
             </div>
             <button type="submit" class="btn">Crear Usuario</button>
         </form>
-</section>
+    </section>
+</div>
    
-    <div id="modalExito" class="modal" style="display: none;">
-        <div class="modal-content">
-            <span class="close">&times;</span>
-            <p>¡Usuario creado exitosamente!</p>
-        </div>
+<div id="modalExito" class="modal" style="display: none;">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <p>¡Acción realizada exitosamente!</p>
     </div>
 </div>
 <script src="../js/scripts.js"></script>
