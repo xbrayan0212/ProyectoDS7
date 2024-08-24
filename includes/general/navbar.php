8e4f3ebacdba150f90070f41ed420bf10bc8d4f1
xbrayan0212 <xbrayan.tobii02@gmail.com>
2024-08-24 10:44:42 -0500

    fix: Modificaciones en la barra de navegación

--- includes/general/navbar.php
+++ includes/general/navbar.php
@@ -8,10 +8,14 @@
         </div>
         <nav class="nav" id="nav-menu">
             <a href="#" class="nav-link">Inicio</a>
+            <?php if ($tipo === 'A'): ?>
+                       <a href="#" class="nav-link"  onclick="loadPage('../../includes/admin/form_create_user.php')">Crear Usuario</a>
+                   <?php endif; ?>
             <div class="user-info" id="user-info">
                <img class="icon-user" src="../../public/imagenes/icons8-user-24.png" alt="hola"> <a class="nav-link" id="user-name"><?php echo htmlspecialchars($_SESSION['usuario']); ?></a>
                <div class="user-dropdown" id="user-dropdown">
                    <a href="#" onclick="loadPage('../../includes/general/form_cambio_password.php')" class="nav-link">Cambiar Contraseña</a>
+
                   <a href="../../usuarios/logout.php" class="nav-link">Salir</a>
                </div>
             </div>
