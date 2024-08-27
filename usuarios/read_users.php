89f8af5d19df11ef18ca3387d9608929b91854f1
xbrayan0212 <xbrayan.tobii02@gmail.com>
2024-08-26 19:54:51 -0500

    feat: Añade funcionalidad para leer usuarios

---
+++ usuarios/read_users.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+include '../../db/conexion_usuario.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $stmt = $conn->prepare("SELECT * FROM usuario");
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    // Crear un array para almacenar los usuarios
+    $users = [];
+    while ($row = $result->fetch_assoc()) {
+        $users[] = $row;
+    }
+} else {
+    echo "Método de solicitud no permitido.";
+}
+?>
