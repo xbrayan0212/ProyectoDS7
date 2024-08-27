d80166a4d79cf530f67325b2ddfef62426275f5b
xbrayan0212 <xbrayan.tobii02@gmail.com>
2024-08-26 19:54:50 -0500

    feat: Mejoras en los scripts de JavaScript

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -127,3 +127,139 @@ function createUser(event) {
 
     closeModal(modalExito, closeModalBtn);
 }
+
+function mostrarDelete(usuario) {
+    const modalExito = document.getElementById("modalExito");
+    const modalError = document.getElementById("modalError");
+    const exitoMensaje = document.getElementById("exitoMensaje");
+    const errorMensaje = document.getElementById("errorMensaje");
+    const closeModalBtns = document.querySelectorAll(".close");
+
+    const formData = new FormData();
+    formData.append('usuario', usuario);
+
+    fetch('../../usuarios/delete_users.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+
+        if (data.success) {
+            exitoMensaje.textContent = data.message; 
+            modalExito.style.display = "block";
+        } else {
+            errorMensaje.textContent = data.message; 
+            modalError.style.display = "block";
+        }
+
+        // Cerrar los modales cuando se hace clic en el botón de cierre
+        closeModalBtns.forEach(btn => {
+            btn.onclick = function() {
+                modalExito.style.display = "none";
+                modalError.style.display = "none";
+                loadPage('../../includes/admin/form_admin_usuarios.php'); 
+            };
+        });
+
+        // Cerrar los modales si se hace clic fuera del contenido del modal
+        window.onclick = function(event) {
+            if (event.target == modalExito || event.target == modalError) {
+                modalExito.style.display = "none";
+                modalError.style.display = "none";
+                loadPage('../../includes/admin/form_admin_usuarios.php'); 
+            }
+        };
+    })
+    .catch(() => {
+        errorMensaje.textContent = "⚠️ Ocurrió un error. Intente nuevamente.";
+        modalError.style.display = "block";
+
+        // Cerrar el modal de error y cargar la nueva página
+        closeModalBtns.forEach(btn => {
+            btn.onclick = function() {
+                modalError.style.display = "none";
+                loadPage('../../includes/admin/form_admin_usuarios.php'); 
+            };
+        });
+
+        window.onclick = function(event) {
+            if (event.target == modalError) {
+                modalError.style.display = "none";
+            }
+        };
+    });
+}
+function openUpdateModal(usuario, tipo) {
+    const modalUpdate = document.getElementById("updateModal_update");
+    const usuarioMostrar = document.getElementById("usuario_mostrar");
+    const selectTipo = document.getElementById("tipo");
+    const inputUsuario = document.getElementById("usuario");
+    const nuevoUsuario = document.getElementById("nuevo_usuario");
+
+    // Rellena el formulario del modal con los valores del usuario seleccionado
+    usuarioMostrar.textContent = usuario;
+    inputUsuario.value = usuario;
+    selectTipo.value = tipo;
+    nuevoUsuario.value = usuario;
+
+    modalUpdate.style.display = "block";
+}
+
+// Cierra el modal al hacer clic en el botón de cerrar
+const closeUpdateModalBtn = document.getElementById("closeModal");
+if (closeUpdateModalBtn) {
+    closeUpdateModalBtn.onclick = () => {
+        document.getElementById("updateModal_update").style.display = "none";
+    };
+}
+
+// Cierra el modal al hacer clic fuera del contenido del modal
+window.onclick = (event) => {
+    const modalUpdate = document.getElementById("updateModal_update");
+    if (event.target === modalUpdate) {
+        modalUpdate.style.display = "none";
+    }
+};
+
+
+function updateUser(event) {
+    event.preventDefault();
+    const usuario = document.getElementById("usuario").value.trim();
+    const nuevoUsuario = document.getElementById("nuevo_usuario").value.trim();
+    const tipo = document.getElementById("tipo").value;
+    const modalExito = document.getElementById("modalExito");
+    const closeModalBtn = document.querySelector(".close");
+
+    clearErrors();
+
+    if (nuevoUsuario.length < 4) {
+        showError("⚠️ El nombre de usuario debe tener más de 4 caracteres.");
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append('usuario', usuario);
+    formData.append('nuevo_usuario', nuevoUsuario);
+    formData.append('tipo', tipo);
+
+    fetch('../../usuarios/update_user.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            modalExito.style.display = "block";
+            document.getElementById("nuevo_usuario").value = "";
+        } else {
+            showError(data.mensaje);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        showError("⚠️ Ocurrió un error. Intente nuevamente.");
+    });
+
+    closeModal(modalExito, closeModalBtn);
+}
