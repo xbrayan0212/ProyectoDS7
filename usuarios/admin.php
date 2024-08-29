8c152efb10f5275115624969e93cfb47fd805679
xbrayan0212 <xbrayan.tobii02@gmail.com>
2024-08-29 15:38:09 -0500

    feat: Actualización en la generación de admin y contraseña

--- usuarios/admin.php
+++ usuarios/admin.php
@@ -4,11 +4,12 @@ include '../db/conexion_usuario.php';
 $usuario = 'admin';
 $new_password = '123';
 $new_hashed_password = password_hash($new_password, PASSWORD_BCRYPT);
+$tipo = 'A';
 
 echo "Hash generado: " . $new_hashed_password . "<br>";
 
-$stmt = $conn->prepare("UPDATE usuario SET password = ? WHERE usuario = ?");
-$stmt->bind_param("ss", $new_hashed_password, $usuario);
+$stmt = $conn->prepare("INSERT INTO usuario (usuario, password, tipo) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $usuario, $new_hashed_password, $tipo);
 $stmt->execute();
 $stmt->close();
 ?>
